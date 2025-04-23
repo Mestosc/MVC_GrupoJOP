fc60fc500b2a2603992e453e1ddd9bff5621202b
Oscar Rodriguez <orodriguezcabaleiro@danielcastelao.org>
2025-04-23 15:38:05 +0200

    Añadiendo informacion

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,7 @@
 import java.lang.module.ModuleDescriptor;
 
 public class Controller {
+    
     public static void main(String[] args) {
         // Instanciamos la vista y el modelo
         View miView = new View();
@@ -33,6 +34,6 @@ public class Controller {
      */
     public int incrementarVelocidad(String matricula, Model miModel) {
         return miModel.incrementarVelocidad(String matricula); /* En el controller al crear el metodo como en este caso es la clase principal que llama a la vista y lo demás y se ejecuta
-        se define model en el metodo main así que mi metodo lo necesita para poder llamar al metodo y pasarlo en la View*/
+        se define model en el metodo main así que mi metodo lo necesita para poder llamar al metodo y pasarlo en la View, una alternativa seria hacer que el model y la view aqui sean atributos de la clase Controller o alguna cosa más con estaticos o así*/
     }
 }
